a38814aa4bfaebb229ce904498eef778f49bf3b8
Abhijay <abhijay3852@gmail.com>
2026-04-11 22:14:31 +0800

    feat: add modules/home/linux/abhijay.nix as linux home-manager entry point

---
+++ modules/home/linux/abhijay.nix
@@ -0,0 +1,19 @@
+{
+  inputs,
+  ...
+}: {
+  home.username = "abhijay";
+  home.homeDirectory = "/home/abhijay";
+  home.stateVersion = "25.11";
+
+  imports = [
+    inputs.noctalia.homeModules.default
+    ../shared/shell.nix
+    ../shared/packages.nix
+    ../shared/nixvim.nix
+    ./desktop.nix
+    ./packages.nix
+  ];
+
+  programs.noctalia-shell.enable = true;
+}
